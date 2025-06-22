aafed53ce3702dcb2ba627c0e03c6b0dd0fa5e45
BlackSecret164 <tienanhchilang12@gmail.com>
2025-06-23 00:06:29 +0700

    branch product id module

--- src/modules/branch_order.module.ts
+++ src/modules/branch_order.module.ts
@@ -5,9 +5,10 @@ import { BranchOrderService } from '../services/branch_order.service';
 import { Order } from '../entities/order_tb.entity';
 import { OrderDetails } from '../entities/order-details.entity';
 import { ProductSize } from 'src/entities/product_size.entity';
+import { ProductBranch } from '../entities/product_branch.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderDetails, ProductSize])],
+  imports: [TypeOrmModule.forFeature([Order, OrderDetails, ProductSize, ProductBranch])],
   controllers: [BranchOrderController],
   providers: [BranchOrderService],
 })
